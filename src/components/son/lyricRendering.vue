81edbd5c6632cf8ebe2af9b0e2b19948836c0e70
V-I-G <3118590779@qq.com>
2023-05-20 09:06:59 +0800

    引入lyric-parser技术栈，实现对歌词的智能化解析

--- src/components/son/lyricRendering.vue
+++ src/components/son/lyricRendering.vue
@@ -2,20 +2,39 @@
   <div></div>
 </template>
 <script>
+  import Lyric from "../../../node_modules/lyric-parser/dist/lyric.js"; //歌词处理工具
+  import { getLyric } from "@/api/getData"; //异步数据获取
   import { mapGetters } from "vuex";
   export default {
     data() {
       return {
-        Lrc: null,
+        lyric: {
+          lines: [],
+        },
       };
     },
     // 计算属性
     computed: {
       ...mapGetters(["playSongId"]),
     },
-    // 创建后生命周期
-    created() {
-      this.Lrc = this.playSongId;
+    // 方法
+    methods: {
+      async renderLrc() {
+        let handler = function (obj) {
+          console.log(obj);
+          // this hanlder called when lineNum change
+        };
+        let res = await getLyric(this.playSongId);
+        this.lyric = new Lyric(res.lrc.lyric, handler);
+      },
+    },
+    // 监听器
+    watch: {
+      playSongId(val) {
+        if (val != -1) {
+          this.renderLrc();
+        }
+      },
     },
   };
 </script>
